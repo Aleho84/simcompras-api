45941478e6eec1326f672c7007ec4b5e85a87066
Alejandro Abraham <alejandro.r.abraham@gmail.com>
2022-09-21 02:28:02 -0300

    CI test

--- src/test/endpoints/products.js
+++ src/test/endpoints/products.js
@@ -1,34 +1,37 @@
 import axios from 'axios'
 import productGenerator from '../generators/product.js'
 
+import 'dotenv/config'
+const SERVER_SWAGGER = process.env.SERVER_SWAGGER
+
 export const getProducts = async () => {
-    const urlRequest = 'http://localhost:8080/api/products'
+    const urlRequest = `${SERVER_SWAGGER}/api/products`
     const response = await axios.get(urlRequest)
     return response.data
 }
 
 export const postProducts = async () => {
-    const urlRequest = 'http://localhost:8080/api/products'
+    const urlRequest = `${SERVER_SWAGGER}/api/products`
     const body = productGenerator()
     const response = await axios.post(urlRequest, body)
     return response.data
 }
 
 export const getProductId = async (idProduct) => {
-    const urlRequest = `http://localhost:8080/api/products/${idProduct}/`
+    const urlRequest = `${SERVER_SWAGGER}/api/products/${idProduct}/`
     const response = await axios.get(urlRequest)
     return response.data
 }
 
 export const putProductId = async (idProduct) => {
-    const urlRequest = `http://localhost:8080/api/products/${idProduct}/`
+    const urlRequest = `${SERVER_SWAGGER}/api/products/${idProduct}/`
     const body = productGenerator()
     const response = await axios.put(urlRequest, body)
     return response.data
 }
 
 export const deleteProductId = async (idProduct) => {
-    const urlRequest = `http://localhost:8080/api/products/${idProduct}/`
+    const urlRequest = `${SERVER_SWAGGER}/api/products/${idProduct}/`
     const response = await axios.delete(urlRequest)
     return response.data
 }
--- src/test/endpoints/users.js
+++ src/test/endpoints/users.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
 import userGenerator from '../generators/user.js'
 
+import 'dotenv/config'
+const SERVER_SWAGGER = process.env.SERVER_SWAGGER
+
 export const postLogInUser = async () => {
-    const urlRequest = 'http://localhost:8080/api/users/login'
+    const urlRequest = `${SERVER_SWAGGER}/api/users/login`
     const body = {
         "email": "pepeargento@mail.com",
         "password": "fatiga"
@@ -12,20 +15,20 @@ export const postLogInUser = async () => {
 }
 
 export const postSignInUser = async () => {
-    const urlRequest = 'http://localhost:8080/api/users/signin'
+    const urlRequest = `${SERVER_SWAGGER}/api/users/signin`
     const body = userGenerator()
     const response = await axios.post(urlRequest, body)
     return response.data
 }
 
 export const getCurrentUser = async () => {
-    const urlRequest = 'http://localhost:8080/api/users/currentUser'
+    const urlRequest = `${SERVER_SWAGGER}/api/users/currentUser`
     const response = await axios.get(urlRequest)
     return response.data
 }
 
 export const getLogOutUser = async () => {
-    const urlRequest = 'http://localhost:8080/api/users/logout'
+    const urlRequest = `${SERVER_SWAGGER}/api/users/logout`
     const response = await axios.get(urlRequest)
     return response.data
 }
